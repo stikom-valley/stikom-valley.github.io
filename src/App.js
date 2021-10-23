0ce43b225c06e867085270163707ed8083d5142d
Nashir Jamali <nashirjamali@gmail.com>
2021-10-23 14:22:06 +0700

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import logo from './assets/logo.png';
+import logo from "./assets/logo.png";
 import PlaySound from "./components/PlaySound";
+import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
+  const [colorTheme, setTheme] = useDarkMode();
+
   return (
     <div className="App">
-      <div className="lg:container lg:mx-auto">
+      <div className="lg:container lg:mx-auto dark:bg-gray-800 relative">
+        <div className="absolute right-0 mt-20">
+          <span
+            onClick={() => setTheme(!colorTheme)}
+            className="w-10 h-10 bg-indigo-500 block rounded-full cursor-pointer shadow-lg curser-pointer text-white flex items-center justify-center"
+          >
+            {colorTheme === "light" ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              </svg>
+            )}
+          </span>
+        </div>
         <div className="flex justify-center">
           <img src={logo} alt="stikomvalley-logo" width="256" />
         </div>
@@ -15,9 +56,7 @@ function App() {
         <div className="mt-8 text-center">
           <p className="font-black text-3xl">Hangout with us, digitally.</p>
           <a href="https://discord.gg/XAaXrKE" target="_blank">
-            <button
-              className="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-purple-700 hover:border-purple-500 rounded discord mt-8"
-            >
+            <button className="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-purple-700 hover:border-purple-500 rounded discord mt-8">
               Join Discord Server
             </button>
           </a>
--- src/components/PlaySound.js
+++ src/components/PlaySound.js
@@ -1,26 +1,73 @@
 import { useState } from "react";
 import Sound from "react-sound";
-import NarutoSong from "../assets/naruto_song.mp3"
+import NarutoSong from "../assets/naruto_song.mp3";
 
 const PlaySound = (
-    handleSongPlaying,
-    handleSongLoading,
-    handleSongFinishedPlaying
+  handleSongPlaying,
+  handleSongLoading,
+  handleSongFinishedPlaying
 ) => {
-    const [isPlaying, setIsPlaying] = useState(false)
-    return (
-        <div>
-            <button className="font-mono text-lg text-gray-800 text-center mt-10" onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? 'Play amazing muzik' : "Please don't stop"}</button>
-            <Sound
-                url={NarutoSong}
-                playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
-                playFromPosition={300 /* in milliseconds */}
-                onLoading={handleSongLoading}
-                onPlaying={handleSongPlaying}
-                onFinishedPlaying={handleSongFinishedPlaying}
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div>
+      <span
+        onClick={() => setIsPlaying(!isPlaying)}
+        className="w-10 h-10 mx-auto mt-20 bg-black block rounded-full cursor-pointer shadow-lg curser-pointer text-white flex items-center justify-center"
+      >
+        {!isPlaying ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
             />
-        </div>
-    )
-}
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z"
+            />
+          </svg>
+        )}
+      </span>
+      <Sound
+        url={NarutoSong}
+        playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
+        playFromPosition={300 /* in milliseconds */}
+        onLoading={handleSongLoading}
+        onPlaying={handleSongPlaying}
+        onFinishedPlaying={handleSongFinishedPlaying}
+      />
+    </div>
+  );
+};
 
-export default PlaySound
+export default PlaySound;
